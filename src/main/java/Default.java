d1fc7b5955c1058721fe8e255c2bc0b4278ddc03
liu.liang <liu.liang@rea-group.com>
2015-07-09 22:01:18 +0800

    default page

--- src/main/java/Default.java
+++ src/main/java/Default.java
@@ -7,14 +7,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class Default extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String errMsg = "Not supported method.";
-        String method = req.getMethod();
-        if ("GET" != method) {
-            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, method);
-        }
+        PrintWriter out = resp.getWriter();
+        out.println("<!DOCTYPE HTML>");
+        out.println("<html>");
+        out.println("<head><title>Default Request Page</title></head>");
+        out.println("<body>");
+        out.println("<h1>This is a default request page.</h1>");
+        out.println("</body>");
+        out.println("</html>");
+        out.flush();
+        out.close();
     }
 }
